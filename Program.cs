2dc31e876345b4d0192ce790e8ed6a1a5f18f371
YaqubAliy02 <91617731+YaqubAliy02@users.noreply.github.com>
2024-03-01 05:50:37 +0500

    The Repository Is Fixed

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿﻿using System;
+
+namespace TarteebTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator ");
+            Console.WriteLine("You can enter any number in this format: (2/2, 2*2....)");
+            Console.Write("Enter your combination:");
+
+            string userStringInput = Console.ReadLine();
+            int operatorIndex = userStringInput.IndexOf('+');
+            if (operatorIndex == -1)
+            {
+                operatorIndex = userStringInput.IndexOf('-');
+            }
+            else if (operatorIndex == -1)
+            {
+                operatorIndex = userStringInput.IndexOf('*');
+            }
+           else if (operatorIndex == -1)
+            {
+                operatorIndex = userStringInput.IndexOf('/');
+            }
+
+            if (operatorIndex == -1)
+            {
+                Console.WriteLine("Operator not found!");
+            }
+            string sign = userStringInput.Substring(operatorIndex, 1);
+
+            int firstNumber = int.Parse(userStringInput.Substring(0, operatorIndex));
+
+            int secondNumber = int.Parse(userStringInput.Substring(operatorIndex + 1));
+
+            int result = sign switch
+            {
+                "+" => firstNumber + secondNumber,
+                "*" => firstNumber * secondNumber,
+                "/" => firstNumber / secondNumber,
+                "-" => firstNumber - secondNumber,
+                _ => throw new ArgumentException("Invalid operator"),
+            };
+
+            Console.WriteLine($"{firstNumber} {sign} {secondNumber} = {result}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
+
 # Calculator
-You can calculate with any sign in this console application
+
+You can calculate any number in this Console Application and this very userfriendly.
+
+Have a look at `Program.cs` and use can see and use this code for free.
+
+1) Console Input/Output 
+
+![image](./assets/inputOutput.png)
+
+2) Checking Sign
+
+![image](./assets/checkingSign.png)
+
+3) Convert 
+
+![image](./assets//converting.png)
+
+3) Pattern Matching 
+
+![image](./assets/switch.png)
+
+4) Demo 
+
+![image](./assets/demo.png)
